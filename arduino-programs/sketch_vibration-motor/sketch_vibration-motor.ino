fa194740920159c9566c20dc8bf210aeb5a0e21b
Kunal Botla <kunalbotla@gmail.com>
2021-10-25 10:53:29 -0400

    Added sequential vibration

--- arduino-programs/sketch_vibration-motor/sketch_vibration-motor.ino
+++ arduino-programs/sketch_vibration-motor/sketch_vibration-motor.ino
@@ -1,10 +1,27 @@
 void setup() {
-  pinMode(12, OUTPUT);    // sets the digital pin 13 as output
+  pinMode(13, OUTPUT);    // sets the digital pin 13 as output
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);  // sets the pin ouputs
 }
 
 void loop() {
-  digitalWrite(12, HIGH); // sets the digital pin 13 on
-  delay(1000);            // waits for a second
-  digitalWrite(12, LOW);  // sets the digital pin 13 off
-  delay(1000);            // waits for a second
+  digitalWrite(13, HIGH); // A on
+  delay(900);             // 
+  digitalWrite(12, HIGH); // B on
+  delay(100);             // 
+  digitalWrite(13, LOW);  // A off
+  delay(900);             // 
+  digitalWrite(11, HIGH); // C on
+  delay(100);             // 
+  digitalWrite(12, LOW);  // B off
+  delay(900);             //
+  digitalWrite(10, HIGH); // D on
+  delay(100);             // 
+  digitalWrite(11, LOW);  // C off
+  delay(900);             //
+  digitalWrite(9, HIGH);  // E on
+  delay(100);             // 
+  digitalWrite(10, LOW);  // D off
 }
