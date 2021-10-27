9379ac42381b6604c5104c7c8038fdeacb1830d8
Kunal Botla <kunalbotla@gmail.com>
2021-10-27 09:23:05 -0400

    Two Arduino Setup

--- arduino-programs/sketch_heart-rate/sketch_heart-rate.ino
+++ arduino-programs/sketch_heart-rate/sketch_heart-rate.ino
@@ -1,43 +1,106 @@
+/*
+  Optical Heart Rate Detection (PBA Algorithm) using the MAX30105 Breakout
+  By: Nathan Seidle @ SparkFun Electronics
+  Date: October 2nd, 2016
+  https://github.com/sparkfun/MAX30105_Breakout
+
+  This is a demo to show the reading of heart rate or beats per minute (BPM) using
+  a Penpheral Beat Amplitude (PBA) algorithm.
+
+  It is best to attach the sensor to your finger using a rubber band or other tightening
+  device. Humans are generally bad at applying constant pressure to a thing. When you
+  press your finger against the sensor it varies enough to cause the blood in your
+  finger to flow differently which causes the sensor readings to go wonky.
+
+  Hardware Connections (Breakoutboard to Arduino):
+  -5V = 5V (3.3V is allowed)
+  -GND = GND
+  -SDA = A4 (or SDA)
+  -SCL = A5 (or SCL)
+  -INT = Not connected
+
+  The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
+  but it will also run at 3.3V.
+*/
+
 #include <Wire.h>
 #include "MAX30105.h"
 
-MAX30105 particleSensor; // initialize MAX30102 with I2C
+#include "heartRate.h"
+
+MAX30105 particleSensor;
+
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+
+float beatsPerMinute;
+int beatAvg;
 
 void setup()
 {
   Serial.begin(115200);
-  while(!Serial); //We must wait for Teensy to come online
-  delay(100);
-  Serial.println("");
-  Serial.println("MAX30102");
-  Serial.println("");
-  delay(100);
+  Serial.println("Initializing...");
+
   // Initialize sensor
-  if (particleSensor.begin(Wire, I2C_SPEED_FAST) == false) //Use default I2C port, 400kHz speed
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
   {
     Serial.println("MAX30105 was not found. Please check wiring/power. ");
     while (1);
   }
+  Serial.println("Place your index finger on the sensor with steady pressure.");
 
-  byte ledBrightness = 70; //Options: 0=Off to 255=50mA
-  byte sampleAverage = 1; //Options: 1, 2, 4, 8, 16, 32
-  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-  int sampleRate = 400; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-  int pulseWidth = 69; //Options: 69, 118, 215, 411
-  int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
+  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
 
-  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+  Wire.begin(); // join i2c bus (address optional for master)
 }
 
-void loop() {
-  particleSensor.check(); //Check the sensor
-  while (particleSensor.available()) {
-      // read stored IR
-      Serial.print(particleSensor.getFIFOIR());
-      Serial.print(",");
-      // read stored red
-      Serial.println(particleSensor.getFIFORed());
-      // read next set of samples
-      particleSensor.nextSample();      
+byte x = 0;
+
+void loop()
+{
+  long irValue = particleSensor.getIR();
+
+  if (checkForBeat(irValue) == true)
+  {
+    //We sensed a beat!
+    long delta = millis() - lastBeat;
+    lastBeat = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0);
+
+    if (beatsPerMinute < 255 && beatsPerMinute > 20)
+    {
+      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+      rateSpot %= RATE_SIZE; //Wrap variable
+
+      //Take average of readings
+      beatAvg = 0;
+      for (byte x = 0 ; x < RATE_SIZE ; x++)
+        beatAvg += rates[x];
+      beatAvg /= RATE_SIZE;
+    }
   }
+
+  Serial.print("IR=");
+  Serial.print(irValue);
+  Serial.print(", BPM=");
+  Serial.print(beatsPerMinute);
+  Serial.print(", Avg BPM=");
+  Serial.print(beatAvg);
+
+  if (irValue < 50000)
+    Serial.print(" No finger?");
+
+  Serial.println();
+
+  Wire.beginTransmission(4); // transmit to device #4
+  Wire.write(beatsPerMinute);   // sends one byte  
+  Wire.endTransmission();    // stop transmitting
+
+  x++;
+  delay(500);
 }
--- arduino-programs/sketch_vibration-motor/sketch_vibration-motor.ino
+++ arduino-programs/sketch_vibration-motor/sketch_vibration-motor.ino
@@ -1,27 +1,55 @@
-void setup() {
-  pinMode(13, OUTPUT);    // sets the digital pin 13 as output
+// Wire Master Writer
+// by Nicholas Zambetti <http://www.zambetti.com>
+
+// Demonstrates use of the Wire library
+// Writes data to an I2C/TWI slave device
+// Refer to the "Wire Slave Receiver" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+
+#include <Wire.h>
+
+void setup()
+{
+
+  pinMode(13, OUTPUT);  // sets the digital pin 13 as output
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(9, OUTPUT);  // sets the pin ouputs
+  
+  Wire.begin(); // join i2c bus (address optional for master)
 }
 
-void loop() {
+byte x = 0;
+
+void loop()
+{
+  Wire.beginTransmission(4);  // transmit to device #4
+  Wire.write("x is ");        // sends five bytes
+  Wire.write(x);              // sends one byte  
+  Wire.endTransmission();     // stop transmitting
+
+  x++;
+  delay(500);
+
   digitalWrite(13, HIGH); // A on
-  delay(900);             // 
-  digitalWrite(12, HIGH); // B on
-  delay(100);             // 
+  delay(1000);            // 
   digitalWrite(13, LOW);  // A off
-  delay(900);             // 
-  digitalWrite(11, HIGH); // C on
-  delay(100);             // 
+  digitalWrite(12, HIGH); // B on
+  delay(1000);            // 
   digitalWrite(12, LOW);  // B off
-  delay(900);             //
-  digitalWrite(10, HIGH); // D on
-  delay(100);             // 
+  digitalWrite(11, HIGH); // C on
+  delay(1000);            // 
   digitalWrite(11, LOW);  // C off
-  delay(900);             //
-  digitalWrite(9, HIGH);  // E on
-  delay(100);             // 
+  digitalWrite(10, HIGH); // D on
+  delay(1000);            //
   digitalWrite(10, LOW);  // D off
+  delay(1000);            // 
+  digitalWrite(9, HIGH);  // E on
+  delay(1000);            // 
+  digitalWrite(9, LOW);   // E off
 }
