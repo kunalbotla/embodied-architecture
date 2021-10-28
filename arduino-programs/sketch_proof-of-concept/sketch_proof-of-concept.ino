2e932d05b1198cb4bae7b139d169d6e2e4de730c
Kunal Botla <kunalbotla@gmail.com>
2021-10-28 10:04:31 -0400

    Uno to Nano and Remove output 5

--- arduino-programs/sketch_proof-of-concept/sketch_proof-of-concept.ino
+++ arduino-programs/sketch_proof-of-concept/sketch_proof-of-concept.ino
@@ -1,26 +1,22 @@
 void setup() {
-  pinMode(12, OUTPUT);  // sets the digital pin 13 as output
-  pinMode(11, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(8, OUTPUT);  // sets the pin ouputs
+  pinMode(5, OUTPUT);  // sets the digital pin 13 as output
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT); // sets the pin ouputs
 }
 
 void loop() {
-  digitalWrite(12, HIGH); // A on
-  delay(400);            // 
-  digitalWrite(12, LOW);  // A off
-  digitalWrite(11, HIGH); // B on
-  delay(1000);            // 
-  digitalWrite(11, LOW);  // B off
-  digitalWrite(10, HIGH); // C on
-  delay(900);            // 
-  digitalWrite(10, LOW);  // C off
-  digitalWrite(9, HIGH); // D on
-  delay(600);            //
-  digitalWrite(9, LOW);  // D off
-  delay(400);            // 
-  digitalWrite(8, HIGH);  // E on
-  delay(200);            // 
-  digitalWrite(8, LOW);   // E off
+  digitalWrite(5, HIGH); // A on
+  delay(random(150, 200));            // 
+  digitalWrite(5, LOW);  // A off
+  digitalWrite(4, HIGH); // B on
+  delay(random(900, 300));            // 
+  digitalWrite(4, LOW);  // B off
+  digitalWrite(3, HIGH); // C on
+  delay(random(800, 900));            // 
+  digitalWrite(3, LOW);  // C off
+  digitalWrite(2, HIGH); // D on
+  delay(random(500, 600));            //
+  digitalWrite(2, LOW);  // D off
+  delay(random(300, 400));            //  
 }
