99a2d0e7c4d2e04d8e223b466e2ae854382a5301
Kunal Botla <kunalbotla@gmail.com>
2021-10-28 10:08:59 -0400

    Reverse Script

--- arduino-programs/sketch_proof-of-concept/sketch_proof-of-concept.ino
+++ arduino-programs/sketch_proof-of-concept/sketch_proof-of-concept.ino
@@ -19,4 +19,18 @@ void loop() {
   delay(random(500, 600));            //
   digitalWrite(2, LOW);  // D off
   delay(random(300, 400));            //  
+
+  digitalWrite(2, HIGH); // A on
+  delay(random(150, 200));            // 
+  digitalWrite(2, LOW);  // A off
+  digitalWrite(3, HIGH); // B on
+  delay(random(900, 300));            // 
+  digitalWrite(3, LOW);  // B off
+  digitalWrite(4, HIGH); // C on
+  delay(random(800, 900));            // 
+  digitalWrite(4, LOW);  // C off
+  digitalWrite(5, HIGH); // D on
+  delay(random(500, 600));            //
+  digitalWrite(5, LOW);  // D off
+  delay(random(300, 400));            // 
 }
